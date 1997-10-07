d4da91014377876146886a7c83f7901ccea8b7ef
Peter Simons <simons@cryp.to>
1997-10-07 00:47:07 +0000

    Added preliminary versions.

---
+++ source/main.c
@@ -0,0 +1,14 @@
+/*
+ *      $Source$
+ *      $Revision$
+ *      $Date$
+ *
+ *      Copyright (C) 1997 by Peter Simons, Germany
+ *      All rights reserved.
+ */
+
+int
+main(int argc, char * argv[])
+{
+    return 0;
+}
