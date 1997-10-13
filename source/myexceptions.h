2f4f91c6a092afe7e3d0bb8332566ff7c227f29f
Peter Simons <simons@cryp.to>
1997-10-13 12:26:57 +0000

    This is the place where my self-defined exceptions will go. mapson does never include the library's include directly.

---
+++ source/myexceptions.h
@@ -0,0 +1,19 @@
+/*
+ *      $Source$
+ *      $Revision$
+ *      $Date$
+ *
+ *      Copyright (C) 1997 by Peter Simons, Germany
+ *      All rights reserved.
+ */
+
+#ifndef __LIB_MY_EXCEPTIONS_H__
+#define __LIB_MY_EXCEPTIONS_H__ 1
+
+#include <exceptions.h>
+
+enum {
+    UNKNOWN_FATAL_EXCEPTION = BEGIN_USER_DEFINED_EXCEPTION
+};
+
+#endif /* !__LIB_MY_EXCEPTIONS_H__ */
