db7514cfc395623dc1f5a838c1d60f362a258307
Peter Simons <simons@cryp.to>
1997-10-13 16:25:03 +0000

    Added array.c module, which implements a dynamically growing array.

--- source/mapson.h
+++ source/mapson.h
@@ -23,4 +23,19 @@ char *get_mail_rescue_filename(void);
 void *fail_safe_malloc(size_t size);
 char *fail_safe_strdup(char *string);
 char *fail_safe_sprintf(const char *fmt, ...  );
+void *fail_safe_realloc(void * ptr, size_t size);
 void fail_safe_fwrite(void *buffer, size_t size, size_t nmemb, FILE *stream);
+
+/* array.c */
+
+struct array_state {
+    char **       array;
+    unsigned int  array_size;
+    unsigned int  array_pos;
+};
+typedef struct array_state * array_t;
+
+array_t build_array(void  );
+void append_to_array(array_t state, char *string);
+char **get_array(array_t state);
+void free_array(char **array);
