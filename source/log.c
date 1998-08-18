db91914d2ec63fc0d7b9ccf58503052f563655e1
Peter Simons <simons@cryp.to>
1998-08-18 12:23:40 +0000

    Addde file locking mechanism around the logfile access.

--- source/log.c
+++ source/log.c
@@ -11,6 +11,9 @@
 #include <stdarg.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
 
 #include <myexceptions.h>
 #include <paths.h>
@@ -24,14 +27,48 @@ log(const char * fmt, ...)
     va_list ap;
 
     if (logfile == NULL) {
-	char * home_dir;
-	char * filename;
+	int           fd;
+	unsigned int  lock_attempts;
+	int           rc = 0;
+	char *        home_dir;
+	char *        filename;
+	struct flock  lock;
 
 	home_dir = get_home_directory();
 	filename = fail_safe_sprintf("%s/%s/logfile", home_dir,
 				     MAPSON_HOME_DIR_PATH);
-	logfile = fopen(filename, "a");
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0600);
+	if (fd == -1) {
+	    log("Failed opening file '%s': %s", filename, strerror(errno));
+	    THROW(IO_EXCEPTION);
+	}
+
+	for (lock_attempts = 0;
+	     lock_attempts == 0 ||
+		 (rc == -1 && (errno == EACCES || errno == EAGAIN) && lock_attempts < 10);
+	     lock_attempts++) {
+	    lock.l_start  = 0;
+	    lock.l_len    = 0;
+	    lock.l_type   = F_WRLCK;
+	    lock.l_whence = SEEK_SET;
+	    if (lock_attempts > 0)
+	      sleep(1);
+	    rc = fcntl(fd, F_SETLK, &lock);
+	}
+
+	if (rc == -1 && errno != EACCES) {
+	    log("Failed to lock file '%s': %s", filename, strerror(errno));
+	    THROW(IO_EXCEPTION);
+	}
+
+	if (lock_attempts >= 10)
+	  log("Couldn't get exclusive lock on '%s'. Writing nonetheless now.",
+	      filename);
+
+	logfile = fdopen(fd, "a");
 	if (logfile == NULL) {
+	    log("Couldn't re-open '%s': %s", filename, strerror(errno));
 	    THROW(IO_EXCEPTION);
 	}
     }
@@ -41,4 +78,5 @@ log(const char * fmt, ...)
     vfprintf(logfile, fmt, ap);
     fprintf(logfile, "\n");
     va_end(ap);
+
 }
