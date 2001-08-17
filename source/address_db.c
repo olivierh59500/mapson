8550266a1d06c377123eff4b67832e7cd0f24025
Peter Simons <simons@cryp.to>
2001-08-17 18:19:50 +0000

    - Include db_185.h instead of db.h. - Include local includes directly.

--- source/address_db.c
+++ source/address_db.c
@@ -11,14 +11,14 @@
 #include <assert.h>
 #include <fcntl.h>
 #include <string.h>
-#include <db.h>
+#include <db_185.h>
 #include <errno.h>
 #ifndef O_EXLOCK
 #  define O_EXLOCK 0
 #endif
 
-#include <myexceptions.h>
-#include <paths.h>
+#include "myexceptions.h"
+#include "paths.h"
 #include "mapson.h"
 
 /********** static variables **********/
