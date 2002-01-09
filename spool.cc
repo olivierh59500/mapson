7d63c33d7062e333ba1473d32f37d3e938bf190d
Peter Simons <simons@cryp.to>
2002-01-09 13:35:52 +0000

    - Try to create the spool directory if it does not exist. - Close file descriptor in case before throwing an exception.

--- spool.cc
+++ spool.cc
@@ -6,8 +6,8 @@
 // POSIX.1 system headers.
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 // My own libraries.
 #include "libmd5/md5.h"
@@ -20,22 +20,26 @@ using namespace std;
 
 string spool(const string& mail)
     {
-    unsigned char sum[16];
-    char buf[33];
-    string filename;
-
     // Calculate the md5 hash of the mail.
 
+    unsigned char sum[16];
+    char buf[33];
     md5_buffer(mail.data(), mail.size(), sum);
     snprintf(buf, 33, "%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
             sum[0], sum[1], sum[2], sum[3], sum[4], sum[5], sum[6], sum[7],
             sum[8], sum[9], sum[10], sum[11], sum[12], sum[13], sum[14], sum[15]);
 
-    filename = config->spool_dir + "/" + buf;
-    debug(("Spooling e-mail as '%s'.", filename.c_str()));
+    // Make sure the spool directory exists.
+
+    struct stat mystat;
+    if (stat(config->spool_dir.c_str(), &mystat) == -1)
+        if (mkdir(config->spool_dir.c_str(), S_IRUSR | S_IWUSR | S_IRWXU) == -1)
+            throw runtime_error(string("Can't create spool directory '") + config->spool_dir + "'");
 
     // Open the spool file and write store the mail there.
 
+    string filename = config->spool_dir + "/" + buf;
+    debug(("Spooling e-mail as '%s'.", filename.c_str()));
     int fd = open(filename.c_str(), O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
     if (fd < 0)
 	throw system_error(string("Can't open spool file '") + filename + "' for writing");
@@ -43,7 +47,10 @@ string spool(const string& mail)
 	{
 	ssize_t rc = write(fd, mail.data()+len, mail.size()-len);
 	if (rc < 0)
+            {
+            close(fd);
 	    throw system_error(string("Failed writing to the spool file '") + filename + "'");
+            }
 	else
 	    len += rc;
 	}
