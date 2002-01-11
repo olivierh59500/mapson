755a7eec3d20b56475c88da5d3cb0719825f457e
Peter Simons <simons@cryp.to>
2002-01-11 21:52:55 +0000

    Implemented the part of mapson that will check whether an incoming e-mail is a confirmation and deliver the spooled mail corresponding to it.

---
+++ accept-confirmation.cc
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2002 by Peter Simons <simons@computer.org>.
+ * All rights reserved.
+ */
+
+// ISO C++ headers.
+#include <vector>
+
+// POSIX.1 system headers.
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+// My own libraries.
+#include "system-error/system-error.hh"
+#include "RegExp/RegExp.hh"
+#include "log.hh"
+#include "config.hh"
+#include "fd-sentry.hh"
+#include "deliver.hh"
+#include "accept-confirmation.hh"
+
+using namespace std;
+
+bool accept_confirmation(const std::string& mail)
+    {
+    // Find all strings in the mail that match the pattern and check,
+    // whether a mail has been spooled under that name. If, deliver
+    // that mail and return.
+
+    const RegExp cookie_pattern("[a-f0-9]{32}", REG_EXTENDED | REG_ICASE);
+    regmatch_t pmatch[2];
+
+    string::size_type pos = 0;
+    while(regexec(cookie_pattern, mail.c_str() + pos, sizeof(pmatch) / sizeof(regmatch_t), pmatch, 0) == 0)
+        {
+        string cookie = mail.substr(pos + pmatch[0].rm_so, pmatch[0].rm_eo - pmatch[0].rm_so);
+        debug(("Found potential cookie '%s' at mail offset %u to offset %u", cookie.c_str(),
+               pmatch[0].rm_so, pmatch[0].rm_eo));
+
+        string filename = config->spool_dir + "/" + cookie;
+        int fd = open(filename.c_str(), O_RDONLY, 0);
+        if (fd >= 0)
+            {
+            fd_sentry s(fd);
+
+            // We found the mail; now deliver it.
+
+            info("Incoming e-mail contained cookie '%s'; delivering the corresponding spooled mail.", cookie.c_str());
+            string buffer;
+            char tmp[8*1024];
+            ssize_t rc;
+            for (rc = read(fd, tmp, sizeof(tmp));
+                 rc > 0;
+                 rc = read(fd, tmp, sizeof(tmp)))
+                {
+                buffer.append(tmp, rc);
+                }
+            if (rc < 0)
+                throw system_error(string("Failed to read mail file '") + filename + "'");
+            deliver(buffer);
+            unlink(filename.c_str());
+            return true;
+            }
+        else
+            pos += pmatch[0].rm_so + 1;
+        }
+
+    debug(("E-mail does not contain a valid cookie; continue processing."));
+    return false;
+    }
