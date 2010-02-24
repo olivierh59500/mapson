b986f5c4ed2c2174159135a03553f78207b512f1
Peter Simons <simons@cryp.to>
2010-02-24 15:31:13 +0100

    configure.ac: use <config.h> header to store Autoconf results

--- config.cpp
+++ config.cpp
@@ -15,6 +15,7 @@
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "config.h"
 #include "mapson.hpp"
 #include "varexp/varexp.hpp"
 #include <stdlib.h>
--- mapson.cpp
+++ mapson.cpp
@@ -15,6 +15,7 @@
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "config.h"
 #include "mapson.hpp"
 #include "rfc822.hpp"
 #include "regexp.hpp"
