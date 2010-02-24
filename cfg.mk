61d396bb3060c20877179df1ade09431d3e5e718
Peter Simons <simons@cryp.to>
2010-02-24 15:32:09 +0100

    NEWS: prepare for 3.3 release

--- cfg.mk
+++ cfg.mk
@@ -5,4 +5,4 @@ upload_dest_dir_:= /home/frs/project/m/ma/mapson
 gpg_key_ID      := 99089D72
 today           := $(date "+%Y-%m-%d")
 TAR_OPTIONS     += --mtime=$(today)
-old_NEWS_hash   := 2a20aa417e6a09dfbed7f08478ac2714
+old_NEWS_hash   := d41d8cd98f00b204e9800998ecf8427e
