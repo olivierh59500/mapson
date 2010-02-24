5afb6586c4d5744cac0a6789e1683ab6494d7c0e
Peter Simons <simons@cryp.to>
2010-02-24 13:24:49 +0100

    Updated administrative files for the 3.2 release.

--- cfg.mk
+++ cfg.mk
@@ -1,6 +1,8 @@
 # mapson/cfg.mk
 
+gnu_rel_host    := peti,mapson@frs.sourceforge.net
+upload_dest_dir_:= /home/frs/project/m/ma/mapson
 gpg_key_ID      := 99089D72
 today           := $(date "+%Y-%m-%d")
 TAR_OPTIONS     += --mtime=$(today)
-old_NEWS_hash   := d41d8cd98f00b204e9800998ecf8427e
+old_NEWS_hash   := e1a91ee7eb2ebf50bbc2ae2cc20bf44d
