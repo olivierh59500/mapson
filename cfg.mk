87315fabed75675440b01327a3d7df734cd70f6f
Peter Simons <simons@cryp.to>
2010-02-24 15:44:08 +0100

    post-release administrivia
    * NEWS: Add header line for next release.
    * .prev-version: Record previous version.
    * cfg.mk (old_NEWS_hash): Auto-update.

--- cfg.mk
+++ cfg.mk
@@ -5,4 +5,4 @@ upload_dest_dir_:= /home/frs/project/m/ma/mapson
 gpg_key_ID      := 99089D72
 today           := $(date "+%Y-%m-%d")
 TAR_OPTIONS     += --mtime=$(today)
-old_NEWS_hash   := d41d8cd98f00b204e9800998ecf8427e
+old_NEWS_hash   := e40ca428afb0985eaf24d9276fd6a8a6
