b61711cc196e832a0faa670ed494d753d3c18903
Peter Simons <simons@cryp.to>
2017-05-22 17:05:36 +0200

    NEWS, bootstrap: update for release

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,8 +27,16 @@ sed -i -e 's/^sc_program_name:/disabled_sc_program_name:/' \
        -e 's/^sc_prohibit_strcmp/disabled_sc_prohibit_strcmp/' \
        -e 's/^sc_require_config_h/disabled_sc_require_config_h/' \
        -e 's/^sc_useless_cpp_parens/disabled_sc_useless_cpp_parens/' \
+       -e 's/^sc_bindtextdomain/disabled_sc_bindtextdomain/' \
+       -e 's/^sc_error_message_period/disabled_sc_error_message_period/' \
+       -e 's/^sc_error_message_uppercase/disabled_sc_error_message_uppercase/' \
+       -e 's/^sc_prohibit_always-defined_macros/disabled_sc_prohibit_always-defined_macros/' \
+       -e 's/^sc_prohibit_path_max_allocation/disabled_sc_prohibit_path_max_allocation/' \
+       -e 's/^sc_prohibit_strncpy/disabled_sc_prohibit_strncpy/' \
+       -e 's/^sc_prohibit_test_minus_ao/disabled_sc_prohibit_test_minus_ao/' \
+       -e 's/^sc_unmarked_diagnostics/disabled_sc_unmarked_diagnostics/' \
   maint.mk
 
 build-aux/gitlog-to-changelog >ChangeLog
 
-autoreconf --install -Wall
+autoreconf --install
