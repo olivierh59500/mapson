fbbd1376b71e9bf561c3eddbba012e031211785f
Peter Simons <simons@cryp.to>
2017-05-22 17:10:40 +0200

    NEWS: fix spelling error

--- cfg.mk
+++ cfg.mk
@@ -5,4 +5,4 @@ upload_dest_dir_:= /home/frs/project/m/ma/mapson
 gpg_key_ID      := 99089D72
 today           := $(date "+%Y-%m-%d")
 TAR_OPTIONS     += --mtime=$(today)
-old_NEWS_hash   := e40ca428afb0985eaf24d9276fd6a8a6
+old_NEWS_hash   := e1966363d25321d86c7b56e916e67397
