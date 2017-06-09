412d6bb8a9b526e990135723c99b93775a7361af
Peter Simons <simons@cryp.to>
2017-06-09 10:09:15 +0200

    Update copyright headers.

--- accept-confirmation.cpp
+++ accept-confirmation.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010 by Peter Simons <simons@cryp.to>.
+ * Copyright (c) 2010-2017 by Peter Simons <simons@cryp.to>.
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
